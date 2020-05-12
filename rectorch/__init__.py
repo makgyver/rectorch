027848a0f970cbd81445b650b3bfff448a70623e
Mirko <mak1788@gmail.com>
2020-05-12 11:35:02 +0200

    Update __init__.py

--- rectorch/__init__.py
+++ rectorch/__init__.py
@@ -1,3 +1,11 @@
 """rectorch: State-of-the-art recsys approaches implemented in pytorch.
 """
 #__all__ = ['configuration', 'data', 'evaluation', 'metrics', 'models', 'nets', 'samplers']
+
+import configuration
+import evaluation
+import data
+import metrics
+import models
+import nets
+import samplers
